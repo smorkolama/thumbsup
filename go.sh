99ad1c9942029b751241fc331f1c64ff53d2ce2d
Benjamin van den Hout <benjamin@nas>
2015-12-10 12:29:11 +0100

    change go script

--- go.sh
+++ go.sh
@@ -1,3 +1,4 @@
 rm -rf /var/www/fototest/public/
 rm /var/www/fototest/*html 
-bin/thumbsup.js --sort-reverse true --title "Gallery" --thumb-size 80  --input /data/webfoto/lokaal --output /var/www/fototest/
+bin/thumbsup.js --original-photos false --sort-reverse true --title "Gallery" --thumb-size 80  --input /data/webfoto/lokaal --output /var/www/fototest/
+cp .htaccess /var/www/fototest
